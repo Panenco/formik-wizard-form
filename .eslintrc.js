96af803f884a991ec98c5fefb2106f8b07c73730
doichev kostia <kostiantyn.doichev@panenco.com>
2022-05-02 17:04:43 +0200

    feat: updated packages and configs
    - configurations were taken from panenco wiki and panenco ui

---
+++ .eslintrc.js
@@ -0,0 +1,68 @@
+const path = require('path');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'airbnb-base',
+    'plugin:prettier/recommended',
+    'plugin:import/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    'jsx-a11y/anchor-is-valid': 0,
+    'react/jsx-filename-extension': 0,
+    'react/jsx-props-no-spreading': 0,
+    'import/prefer-default-export': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    'import/no-extraneous-dependencies': 0,
+    'react/display-name': 0,
+    'react/prop-types': 0,
+    'import/extensions': 0,
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    'no-use-before-define': 'off',
+    '@typescript-eslint/no-use-before-define': ['error'],
+    'react/require-default-props': 'off', // Since we do not use prop-types
+    quotes: [2, 'single', { avoidEscape: true }],
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: [path.resolve(__dirname, './node_modules'), path.resolve(__dirname, 'src')],
+      },
+    },
+  },
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
+  ],
+};
